b02d35f3638986f6f8fd610a64c2788fe19457c4
cjgarcia12 <christian.jgarcia03@gmail.com>
2024-06-13 09:32:01 -0400

    My submission for Rock, Paper, Scissors Part 1

---
+++ rockPaperScissors.js
@@ -0,0 +1,53 @@
+const hands = ['rock' , 'paper', 'scissors']; 
+
+function getHand() {
+    let hand = parseInt(Math.random()*10)%3;
+    if (hand === 0) {
+        return 'Rock';
+    } else if (hand === 1) { 
+        return 'Paper';
+    } else  if (hand === 2) {
+        return 'Scissors';
+    } 
+}
+
+let players = {
+    player1: {
+        name: 'player 1',
+        return: getHand()
+    },
+    player2: {
+        name: 'player 2',
+        return: getHand()
+    }
+}
+
+function playRound() {
+    if (players.player1.return === players.player2.return) {
+
+        console.log(`${players.player1.name} played ${players.player1.return}`);
+        console.log(`${players.player2.name} played ${players.player2.return}`);
+        console.log('It is a Tie!');
+        return null;
+
+    } else if (
+
+        (players.player1.return === 'Rock' && players.player2.return === 'Paper') || 
+        (players.player1.return === 'Scissors' && players.player2.return === 'Rock') ||
+        (players.player1.return === 'Paper' && players.player2.return === 'Scissors')
+    ) {
+
+        console.log(`${players.player1.name} played ${players.player1.return}`);
+        console.log(`${players.player2.name} played ${players.player2.return}`);
+        console.log('Player 2 Wins!!')
+        return players.player2.name;
+    } else {
+
+        console.log(`${players.player1.name} played ${players.player1.return}`);
+        console.log(`${players.player2.name} played ${players.player2.return}`);
+        console.log('Player 1 Wins!!')
+        return players.player1.name;
+    }
+}
+
+playRound();
