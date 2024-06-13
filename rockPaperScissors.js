46a01a9fbf94f7c06085e03df1fc01a9dff142d9
cjgarcia12 <christian.jgarcia03@gmail.com>
2024-06-13 14:36:18 -0400

    made it more efficient

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -14,40 +14,67 @@ function getHand() {
 let players = {
     player1: {
         name: 'player 1',
-        return: getHand()
+        getHand: getHand
     },
     player2: {
         name: 'player 2',
-        return: getHand()
+        getHand: getHand
     }
 }
 
 function playRound() {
-    if (players.player1.return === players.player2.return) {
+    let player1Play = players.player1.getHand();
+    let player2Play = players.player2.getHand();
 
-        console.log(`${players.player1.name} played ${players.player1.return}`);
-        console.log(`${players.player2.name} played ${players.player2.return}`);
+    if (player1Play === player2Play) {
+
+        console.log(`${players.player1.name} played ${player1Play}`);
+        console.log(`${players.player2.name} played ${player2Play}`);
         console.log('It is a Tie!');
         return null;
 
     } else if (
 
-        (players.player1.return === 'Rock' && players.player2.return === 'Paper') || 
-        (players.player1.return === 'Scissors' && players.player2.return === 'Rock') ||
-        (players.player1.return === 'Paper' && players.player2.return === 'Scissors')
+        (player1Play === 'Rock' && player2Play === 'Paper') || 
+        (player1Play === 'Scissors' && player2Play === 'Rock') ||
+        (player1Play === 'Paper' && player2Play === 'Scissors')
     ) {
 
-        console.log(`${players.player1.name} played ${players.player1.return}`);
-        console.log(`${players.player2.name} played ${players.player2.return}`);
+        console.log(`${players.player1.name} played ${player1Play}`);
+        console.log(`${players.player2.name} played ${player2Play}`);
         console.log('Player 2 Wins!!')
         return players.player2.name;
     } else {
 
-        console.log(`${players.player1.name} played ${players.player1.return}`);
-        console.log(`${players.player2.name} played ${players.player2.return}`);
+        console.log(`${players.player1.name} played ${player1Play}`);
+        console.log(`${players.player2.name} played ${player2Play}`);
         console.log('Player 1 Wins!!')
         return players.player1.name;
     }
+    // switch(getHand) {
+    //     case (players.player1.return === players.player2.return):
+    //         console.log(`${players.player1.name} played ${players.player1.return}`);
+    //         console.log(`${players.player2.name} played ${players.player2.return}`);
+    //         console.log('It is a Tie!');
+    //         return null;
+        
+    //     case (
+    //         (players.player1.return === 'Rock' && players.player2.return === 'Paper') || 
+    //         (players.player1.return === 'Scissors' && players.player2.return === 'Rock') ||
+    //         (players.player1.return === 'Paper' && players.player2.return === 'Scissors')
+    //     ): 
+    //         console.log(`${players.player1.name} played ${players.player1.return}`);
+    //         console.log(`${players.player2.name} played ${players.player2.return}`);
+    //         console.log('Player 2 Wins!!')
+    //         return players.player2.name;
+    //     default:
+    //         console.log(`${players.player1.name} played ${players.player1.return}`);
+    //         console.log(`${players.player2.name} played ${players.player2.return}`);
+    //         console.log('Player 1 Wins!!')
+    //         return players.player1.name;
+    
+    // }
 }
+
 
 playRound();
