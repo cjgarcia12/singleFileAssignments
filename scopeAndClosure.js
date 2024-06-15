644451d1eb6c3329315a5eab34209c1605281c7e
cjgarcia12 <christian.jgarcia03@gmail.com>
2024-06-14 22:48:15 -0400

    Dunno

---
+++ scopeAndClosure.js
@@ -0,0 +1,17 @@
+function outer(){
+  let a = '';
+  let b = {object: 'value'};
+  console.log(`${a} ${b}`);
+
+    function inner(a, b) {
+        a = 'new';
+        b = {newObject: 'new value'};
+        b.newObject = 'even newer value';
+        console.log(`${a} ${b}`);
+    }
+  
+    inner(a, b);
+    console.log(`${a} ${b}`);
+  }
+  
+  outer();
