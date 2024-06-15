668e7f5bd03a4700092cc849be0a83b6fb6acd9b
cjgarcia12 <christian.jgarcia03@gmail.com>
2024-06-14 22:47:46 -0400

    submission for rockPaperScissors

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -14,15 +14,19 @@ function getHand() {
 let players = {
     player1: {
         name: 'player 1',
-        getHand: getHand
+        getHand: getHand,
+        points: 0
     },
     player2: {
         name: 'player 2',
-        getHand: getHand
+        getHand: getHand,
+        points: 0
     }
 }
 
-function playRound() {
+function playRound(player1, player2) {
+    players.player1.name = player1;
+    players.player2.name = player2;
     let player1Play = players.player1.getHand();
     let player2Play = players.player2.getHand();
 
@@ -42,39 +46,52 @@ function playRound() {
 
         console.log(`${players.player1.name} played ${player1Play}`);
         console.log(`${players.player2.name} played ${player2Play}`);
-        console.log('Player 2 Wins!!')
+        console.log(`${players.player2.name} wins!!`);
         return players.player2.name;
     } else {
 
         console.log(`${players.player1.name} played ${player1Play}`);
         console.log(`${players.player2.name} played ${player2Play}`);
-        console.log('Player 1 Wins!!')
+        console.log(`${players.player1.name} wins!!`)
         return players.player1.name;
     }
-    // switch(getHand) {
-    //     case (players.player1.return === players.player2.return):
-    //         console.log(`${players.player1.name} played ${players.player1.return}`);
-    //         console.log(`${players.player2.name} played ${players.player2.return}`);
-    //         console.log('It is a Tie!');
-    //         return null;
-        
-    //     case (
-    //         (players.player1.return === 'Rock' && players.player2.return === 'Paper') || 
-    //         (players.player1.return === 'Scissors' && players.player2.return === 'Rock') ||
-    //         (players.player1.return === 'Paper' && players.player2.return === 'Scissors')
-    //     ): 
-    //         console.log(`${players.player1.name} played ${players.player1.return}`);
-    //         console.log(`${players.player2.name} played ${players.player2.return}`);
-    //         console.log('Player 2 Wins!!')
-    //         return players.player2.name;
-    //     default:
-    //         console.log(`${players.player1.name} played ${players.player1.return}`);
-    //         console.log(`${players.player2.name} played ${players.player2.return}`);
-    //         console.log('Player 1 Wins!!')
-    //         return players.player1.name;
-    
-    // }
+}
+
+const playGame = (player1, player2, playerUntil) => {
+    let roundNum = 1;
+    players.player1.points = 0;
+    players.player2.points = 0;
+    while(players.player1.points < playerUntil && players.player2.points < playerUntil) {
+        console.log(`---THIS IS ROUND ${roundNum++}---`)
+        let winner = playRound(player1, player2);
+        if (winner === players.player1.name) {
+            players.player1.points++
+        } else if(winner === players.player2.name) {
+            players.player2.points++
+        }
+    }
+    if (players.player1.points < players.player2.points) {
+        return players.player2.name;
+    } else if (players.player1.points > players.player2.points) {
+        return players.player1.name;
+    } 
+}
+
+const playTournament = (player1, player2, player3, player4, playerUntil) => {
+    let winner1 = playGame(player1,player2,playerUntil);
+    let winner2 = playGame(player3,player4,playerUntil);
+
+    let champion = playGame(winner1,winner2,playerUntil);
+
+    return champion;
 }
 
 
-playRound();
+let test1 = 'Christian';
+let test2 = 'Peyton';
+let test3 = 'Sydney';
+let test4 = 'Carlos';
+// playRound();
+// console.log(playGame(test1, test2, 5));
+
+console.log(`${playTournament(test1, test2, test3, test4, 3)} is the world champion!`);
